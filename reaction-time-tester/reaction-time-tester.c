2d31701564a712365bb1fb0879e2150ba418b2a7
heislera763 <heislera763@gmail.com>
2023-08-11 13:42:27 -0400

    moving "default settings" to a header file in case it grows any larger, might revert if I dont like it

--- reaction-time-tester/reaction-time-tester.c
+++ reaction-time-tester/reaction-time-tester.c
@@ -3,20 +3,9 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "Resource.h"
+#include "resource.h"
+#include "default_settings.h"
 
-#define DEFAULT_MIN_DELAY 1000
-#define DEFAULT_MAX_DELAY 3000
-#define DEFAULT_EARLY_RESET_DELAY 3000
-#define DEFAULT_VIRTUAL_DEBOUNCE 50
-#define DEFAULT_NUM_TRIALS 5
-#define DEFAULT_RAWKEYBOARDENABLE 1
-#define DEFAULT_RAWMOUSEENABLE 1
-#define DEFAULT_FONT_SIZE 32
-#define DEFAULT_FONT_NAME L"Arial"
-#define DEFAULT_FONT_STYLE L"Regular"
-#define DEFAULT_RESOLUTION_WIDTH 1280
-#define DEFAULT_RESOLUTION_HEIGHT 720
 #define TIMER_READY 1
 #define TIMER_REACT 2
 #define TIMER_EARLY 3
